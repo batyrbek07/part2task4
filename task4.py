c039e56b5b899c2e8c67e8e396f3ed8c3b89758b
batyrbek <batyrbek071194@gmail.com>
2019-12-21 13:59:14 +0600

    task4

---
+++ task4.py
@@ -0,0 +1,12 @@
+# hour1 = int(input())
+# min1 = int(input())
+# sec1 = int(input())
+# hour2 = int(input())
+# min2 = int(input())
+# sec2 = int(input())
+# # res = ((hour2-hour1)*3600 + (min2-min1)*60 + sec2 - sec1)
+# # print(abs(res))
+
+# total1 = min1* 60 + hour1 * 3600 + sec1
+# total2 = min2 *60 + hour2 * 3600 + sec2
+# print(total2 - total1)
